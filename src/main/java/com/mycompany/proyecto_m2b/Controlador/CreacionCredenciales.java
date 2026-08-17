27be714c6257294159515fb992cacd35a2d4da5d
HP <HP@Kozy>
2026-08-17 12:07:40 -0500

    aads

--- src/main/java/com/mycompany/proyecto_m2b/Controlador/CreacionCredenciales.java
+++ src/main/java/com/mycompany/proyecto_m2b/Controlador/CreacionCredenciales.java
@@ -25,6 +25,7 @@ public class CreacionCredenciales {
     //Hola    
     //123
     //Casa
+    //asjdadsoasijoadsijo
     public static String GenerarUsuario (String Nombre1, String Apellido1){
         char inicialNombre = Character.toUpperCase(Nombre1.charAt(0));
         char inicialApellido = Character.toUpperCase(Apellido1.charAt(0));
