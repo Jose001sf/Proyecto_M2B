f6a8b792cf6287e197ec3684dd7d5cde6549aa21
jose <jose@localhost>
2026-08-25 10:34:28 -0500

    COMMIT

--- src/main/java/com/mycompany/proyecto_m2b/Vista/PanelProveedores.java
+++ src/main/java/com/mycompany/proyecto_m2b/Vista/PanelProveedores.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JPanel.java to edit this template
- */
+
 package com.mycompany.proyecto_m2b.Vista;
 
 import com.mycompany.proyecto_m2b.Controlador.CompraDAO;
