a1ce993cb6e89538b87f2962001fa2e27ad8ee9a
jose <jose@192.168.110.132>
2026-08-30 16:06:23 -0500

    COMMIT

--- src/main/java/com/mycompany/proyecto_m2b/Vista/NuevoResiduo.java
+++ src/main/java/com/mycompany/proyecto_m2b/Vista/NuevoResiduo.java
@@ -8,6 +8,7 @@ import com.mycompany.proyecto_m2b.Controlador.ResiduoDAO;
 import com.mycompany.proyecto_m2b.modelo.Residuos;
 import java.util.List;
 
+
 /**
  *
  * @author jose
@@ -177,6 +178,9 @@ public class NuevoResiduo extends javax.swing.JPanel {
         jLabel7 = new javax.swing.JLabel();
         comboEstadoResiduo = new javax.swing.JComboBox<>();
         ValidarCantidad = new javax.swing.JLabel();
+        btnModificar = new javax.swing.JPanel();
+        Editar = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
 
         Fondo.setBackground(new java.awt.Color(255, 255, 255));
         Fondo.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
@@ -328,6 +332,50 @@ public class NuevoResiduo extends javax.swing.JPanel {
         ValidarCantidad.setText("jLabel1");
         Fondo.add(ValidarCantidad, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 160, 230, 20));
 
+        btnModificar.setBackground(new java.awt.Color(255, 255, 255));
+        btnModificar.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(187, 187, 187)));
+        btnModificar.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        btnModificar.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                btnModificarMouseClicked(evt);
+            }
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                btnModificarMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                btnModificarMouseExited(evt);
+            }
+        });
+
+        Editar.setFont(new java.awt.Font("Roboto", 0, 18)); // NOI18N
+        Editar.setText("Modificar");
+
+        jLabel2.setBackground(new java.awt.Color(255, 255, 255));
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/edit_22dp_000000_FILL0_wght400_GRAD0_opsz24.png"))); // NOI18N
+
+        javax.swing.GroupLayout btnModificarLayout = new javax.swing.GroupLayout(btnModificar);
+        btnModificar.setLayout(btnModificarLayout);
+        btnModificarLayout.setHorizontalGroup(
+            btnModificarLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(btnModificarLayout.createSequentialGroup()
+                .addGap(12, 12, 12)
+                .addComponent(jLabel2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(Editar)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        btnModificarLayout.setVerticalGroup(
+            btnModificarLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, btnModificarLayout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(btnModificarLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(Editar)
+                    .addComponent(jLabel2))
+                .addContainerGap())
+        );
+
+        Fondo.add(btnModificar, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 240, 140, -1));
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
@@ -427,9 +475,26 @@ public class NuevoResiduo extends javax.swing.JPanel {
         cargarComboTiposResiduo();
     }//GEN-LAST:event_comboResiduosMouseClicked
 
+    private void btnModificarMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnModificarMouseClicked
+        
+    }//GEN-LAST:event_btnModificarMouseClicked
+
+    private void btnModificarMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnModificarMouseEntered
+        // TODO add your handling code here:
+        btnModificar.setBackground(new java.awt.Color(219,219,219));
+        Editar.setForeground(new java.awt.Color(66, 66, 66));
+    }//GEN-LAST:event_btnModificarMouseEntered
+
+    private void btnModificarMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnModificarMouseExited
+        // TODO add your handling code here:
+        btnModificar.setBackground(java.awt.Color.white);
+        Editar.setForeground(java.awt.Color.black);
+    }//GEN-LAST:event_btnModificarMouseExited
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel BarraArriba;
+    private javax.swing.JLabel Editar;
     private javax.swing.JPanel Fondo;
     private javax.swing.JLabel Guardar;
     private javax.swing.JLabel ImagenADD1;
@@ -442,8 +507,10 @@ public class NuevoResiduo extends javax.swing.JPanel {
     private javax.swing.JLabel Residuo;
     private javax.swing.JLabel TituloFuncion2;
     private javax.swing.JLabel ValidarCantidad;
+    private javax.swing.JPanel btnModificar;
     private javax.swing.JComboBox<String> comboEstadoResiduo;
     private javax.swing.JComboBox<String> comboResiduos;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel7;
     private javax.swing.JTextField txtCantidad;
     // End of variables declaration//GEN-END:variables
